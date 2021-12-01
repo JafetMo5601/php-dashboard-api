226edc6e6652f78264472b12de9097ab6d34e482
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-11-30 21:12:14 -0600

    Remove the headers file usage and put only the necessary ones. Return messages from HTTP requests.

--- api/user/register.php
+++ api/user/register.php
@@ -1,8 +1,13 @@
 <?php
 
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Origin: * ");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 include_once '../config/db.php';
 include_once '../objects/user.php';
-include_once '../config/header.php';
 
 $db = new Database();
 $conn = $db->getConnection();
@@ -10,25 +15,37 @@ $conn = $db->getConnection();
 $newUser = new User($conn);
 $data = json_decode(file_get_contents("php://input"));
 
+$input_name = $data->name;
+$input_last = $data->last;
+$input_username = $data->username;
+$input_email = $data->email;
+$input_pass = $data->password;
+
 if(
-    !empty($data->name) &&
-    !empty($data->last) &&
-    !empty($data->username) &&
-    !empty($data->email) &&
-    !empty($data->password)
+    !empty($input_name) &&
+    !empty($input_last) &&
+    !empty($input_username) &&
+    !empty($input_email) &&
+    !empty($input_pass)
 ){
-    $newUser->name = $data->name;
-    $newUser->last = $data->last;
-    $newUser->username = $data->username;
-    $newUser->email = $data->email;
-    $newUser->password = $data->password;
+    $newUser->name = $input_name;
+    $newUser->last = $input_last;
+    $newUser->username = $input_username;
+    $newUser->email = $input_email;
+    $newUser->password = $input_pass;
   
     if($newUser->register()){
         http_response_code(201);
-        echo json_encode(array("message" => "User was registered."));
+        echo json_encode(
+            array(
+                "message" => "User was registered."
+            ));
     } else{
         http_response_code(503);
-        echo json_encode(array("message" => "Unable to registed user."));
+        echo json_encode(
+            array(
+                "message" => "Unable to registed user."
+            ));
     }
 } else{
     http_response_code(400);
--- api/user/signin.php
+++ api/user/signin.php
@@ -1,8 +1,13 @@
 <?php 
 
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Origin: * ");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 include_once '../config/db.php';
 include_once '../objects/user.php';
-include_once '../config/header.php';
 
 require __DIR__ . '../../../vendor/autoload.php';
 
@@ -12,10 +17,9 @@ $db = new Database();
 $conn = $db->getConnection();
 
 $registeredUser = new User($conn);
-
 $data = json_decode(file_get_contents("php://input"));
 
-$input_username = $data->email;
+$input_username = $data->username;
 $input_pass = $data->password;
 
 if(
